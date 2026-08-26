cb8e18392c5e9960e483ca365230dc727ef8ec3d
Juan Vitor <juanriven10@gmail.com>
2026-08-26 15:15:26 -0300

    📽️ Sistema de Câmera [ADIADO]
    Comecei bem pouco a fazer o sistema de câmera (movimentar a camera por um ambiente) e foi me informado que tem uma demanda maior pra inspeção de modelo, então to commitando o pouco que eu fiz pra quando voltar a ser prioridade, eu continuar de onde parei.
    
    - Incluí o Cinemachine no projeto
    - Criei um pasta "Examples" na pasta "Scenes" para armazenar todas as cenas que existem apenas para exemplificar utilização de algum sistema

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// Essa classe não está acabada, favor não utilizar por hora!
+/// </summary>
+public class CameraController : MonoBehaviour {
+    public Camera mainCamera;
+
+}
