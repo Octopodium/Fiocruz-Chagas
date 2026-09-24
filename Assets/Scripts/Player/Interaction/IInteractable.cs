1ab4398abce406b4dcc32ee45cd392c1d39ac3b6
Pedro de Lima Birchal (Lima) <birchalpedro@gmail.com>
2026-09-24 15:25:06 -0300

    Gizmos nos Interagiveis

--- Assets/Scripts/Player/Interaction/IInteractable.cs
+++ Assets/Scripts/Player/Interaction/IInteractable.cs
@@ -1,4 +1,6 @@
 
+using UnityEngine;
+
 /// <summary>
 /// Abstract class that defines an object as an Interactable (clickable by the player).
 /// </summary>
@@ -8,5 +10,16 @@ public abstract class IInteractable: IUnderMouse {
     /// Called when an Interactable is interacted with (clicked). It will only be called if CanInteract return true.
     /// </summary>
     public abstract void HandleInteract();
+
+    #if UNITY_EDITOR
+    protected virtual void OnDrawGizmos()
+    {
+        BoxCollider collider = GetComponent<BoxCollider>();
+        Gizmos.color = CanBeFound() ? Color.cyan : Color.red;
+        Gizmos.color *= new Color(1, 1, 1, 0.2f);
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawCube(collider.center , collider.size * 1.05f);
+    }
+    #endif
 
 }
